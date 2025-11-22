547c7e3a1fd4fe5bb713178e587631ef756a3d36
deekshithmd <deekshithmogra@gmail.com>
2025-11-22 13:00:12 +0530

    updated package.json

--- src/app.js
+++ src/app.js
@@ -5,8 +5,26 @@ import authRoutes from "./routes/authRoutes.js";
 import carRoutes from "./routes/carRoutes.js";
 import uploadRoute from "./routes/uploadRoute.js";
 
+const allowedOrigins = ['http://localhost:5173', 'https://ganesh-cars-frontend.vercel.app'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // allow requests with no origin (like Postman & mobile apps)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,         // allow cookies & authorization headers
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+};
+
+
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 
